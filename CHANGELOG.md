fbb1d6ab8b37246fb8035647334e12384a6f2a24
Leandro Santos <>
2023-12-15 17:45:00 -0300

    Bump to v0.4.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,8 @@ Given a version number MAJOR.MINOR.PATCH, increment:
 
 
 ## [Unreleased]
+
+## [0.4.2] - 2023-12-15
 ### Changed
 - Global objects variables
 
--- starkbank/settings.go
+++ starkbank/settings.go
@@ -5,7 +5,7 @@ import (
 	"github.com/starkinfra/core-go/starkcore/utils/hosts"
 )
 
-var SdkVersion = "0.4.1"
+var SdkVersion = "0.4.2"
 var Timeout = 15
 var ApiVersion = "v2"
 var Host = hosts.Bank
