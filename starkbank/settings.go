860dbe602b28624b3596f3653434f0a7ccebaa0a
nicolasalmeida-stark <nicolas.almeida@starkbank.com>
2025-10-02 20:57:58 -0300

    Bump to v1.0.0

--- starkbank/settings.go
+++ starkbank/settings.go
@@ -5,7 +5,7 @@ import (
 	"github.com/starkinfra/core-go/starkcore/utils/hosts"
 )
 
-var SdkVersion = "0.6.0"
+var SdkVersion = "1.0.0"
 var Timeout = 15
 var ApiVersion = "v2"
 var Host = hosts.Bank
