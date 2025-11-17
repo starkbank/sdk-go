5e14fdd0095ac4468b2e90c9f5e07e0ecdc76ead
nicolasalmeida-stark <nicolas.almeida@starkbank.com>
2025-11-17 16:38:21 -0300

    Fix to corporate balance test

--- tests/sdk/corporate_balance_test.go
+++ tests/sdk/corporate_balance_test.go
@@ -2,7 +2,7 @@ package sdk
 
 import (
 	"github.com/starkbank/sdk-go/starkbank"
-	"github.com/starkbank/sdk-go/starkbank/balance"
+	"github.com/starkbank/sdk-go/starkbank/corporatebalance"
 	Utils "github.com/starkbank/sdk-go/tests/utils"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -12,7 +12,7 @@ func TestCorporateBalanceGet(t *testing.T) {
 
 	starkbank.User = Utils.ExampleProject
 
-	balance, err := balance.Get(nil)
+	balance, err := corporatebalance.Get(nil)
 	if err.Errors != nil {
 		for _, e := range err.Errors {
 			t.Errorf("code: %s, message: %s", e.Code, e.Message)
